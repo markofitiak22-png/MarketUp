6075b122915471eef06e697b40a95aec6a26ed16
markofitiak22-png <markofitiak22@gmail.com>
2025-10-22 15:16:35 +0300

    fix deploy

--- marketup/next.config.ts
+++ marketup/next.config.ts
@@ -3,10 +3,7 @@ import path from "path";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
-  experimental: {
-    // Silence workspace root tracing warning when multiple lockfiles exist
-    outputFileTracingRoot: path.join(__dirname),
-  },
+  outputFileTracingRoot: path.join(__dirname),
 };
 
 export default nextConfig;
